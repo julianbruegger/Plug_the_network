13c94904f28f882bd209286a44742109c4bd6dc7
Julian <julian.bruegger@ict-bz.ch>
2019-11-05 09:24:03 +0100

    Signed-off-by: Julian <julian.bruegger@ict-bz.ch>

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-git clone https://github.com/julianbruegger/Plug_the_network.git
+git fetch https://github.com/julianbruegger/Plug_the_network.git
 sudo python /home/pi/Plug_The_Network/Colorwipe.py
